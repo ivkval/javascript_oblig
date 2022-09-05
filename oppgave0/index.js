6601724f9d489f7587dee0c678636fd65613b19a
Ivar Kvalheim <ivarball1@gmail.com>
2022-09-05 15:26:13 +0200

    Added some comments in task 0 index.js

--- oppgave0/index.js
+++ oppgave0/index.js
@@ -1,27 +1,43 @@
 // Oppgave 1
+// Henter teksten som skal fjernes
 const removableText = document.getElementById("remove");
+// Henter knappen som skal fjerne teksten
 const removeTextButton = document.getElementById("remove-btn");
 
+// Fester en klikk-lytter som fjerner teksten med
+// en funskjon som fjerner teksten
 removeTextButton.addEventListener("click", () => {
   removableText.innerHTML = "";
 });
 
 // Oppgave 2
-const newText = `“It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair.”
+// Tekst som skal byttes ut med den som allerede finnes på siden.
+const newText = `“It was the best of times, it was the worst of times, 
+                  it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, 
+                  it was the epoch of incredulity, it was the season of light, it was the season of darkness, 
+                  it was the spring of hope, it was the winter of despair.”
 
                   ― Charles Dickens, A Tale of Two Cities`;
+
+// Den gamle teksten som skal byttes ut
 const changeableText = document.getElementById("change");
+// Knappen som skal bytte tekstene
 const changeTextButton = document.getElementById("change-btn");
 
+// Klikk-lytter som legger til den nye teksten på siden
 changeTextButton.addEventListener("click", () => {
   changeableText.innerHTML = newText;
 });
 
 // Oppgave 3
+// Input-feltet som man skriver i
 const inputField = document.getElementById("input");
+// Tekstboksen som skriver ut det som skrives i input-feltet
 const inputText = document.getElementById("input-text");
 
+// En lytter som kobles til input-feltet og som lytter etter inputs
 inputField.addEventListener("input", (e) => {
+  // Henter teksten og skriver den ut for hvert tastetrykk
   let text = e.target.value;
   inputText.innerHTML = text;
 });
